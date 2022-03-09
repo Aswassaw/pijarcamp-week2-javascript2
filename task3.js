1cfab58f6e7e17c78fa110ed5a2a5493918e9674
Andry Pebrianto <andrypeb227@gmail.com>
2022-03-09 21:31:30 +0700

    Menyelesaikan task3

---
+++ task3.js
@@ -0,0 +1,35 @@
+const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
+  // validasi nilaiAwal < nilaiAkhir
+  if (nilaiAwal < nilaiAkhir) {
+    // validasi dataArray harus array
+    if (Array.isArray(dataArray)) {
+      // validasi panjang dataArray > 5
+      if (dataArray.length > 5) {
+        return filterData(nilaiAwal, nilaiAkhir, dataArray);
+      } else {
+        return "Jumlah angka dalam dataArray harus lebih dari 5";
+      }
+    } else {
+      return "Parameter ketiga harus bertipe array";
+    }
+  } else {
+    return "Nilai akhir harus lebih besar dari nilai awal";
+  }
+};
+
+const filterData = (nilaiAwal, nilaiAkhir, dataArray) => {
+  const hasil = dataArray
+    .filter((item) => {
+      if (item >= nilaiAwal && item <= nilaiAkhir) {
+        return item;
+      }
+    })
+    .sort((a, b) => a - b);
+
+  return hasil;
+};
+
+console.log(seleksiNilai(15, 3, [2, 25, 4, 14, 17, 30, 8]));
+// console.log(seleksiNilai(5, 17, [2, 25, 4]));
+// console.log(seleksiNilai(7, 8, undefined));
+// console.log(seleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]));
